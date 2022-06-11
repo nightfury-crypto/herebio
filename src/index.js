79c1047a1dc668c34eab848cb7fec55d1c770451
KARNAIL <pradhankarnail@gmail.com>
2022-06-11 14:02:05 +0530

    added height property

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter  } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 
+document.querySelector('body').style.height = window.innerHeight;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <HashRouter >
