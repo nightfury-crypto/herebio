615286db79c892e2aa0e82136c2ef21a25dc5253
Karnail Singh Choudhary <pradhankarnail@gmail.com>
2022-09-26 20:18:39 +0530

    meta tags added
    Signed-off-by: Karnail Singh Choudhary <pradhankarnail@gmail.com>

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import Footer from './footer/Footer';
 
 
 function App() {
-
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 
--- src/landingpage/LandingPage.js
+++ src/landingpage/LandingPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
     return (
@@ -7,6 +8,9 @@ const LandingPage = () => {
                 <h1>HERE BIO</h1>
                 <h4>It's a website where you can find some tools related to biology</h4>
                 <h5>Select the service from the sidebar.</h5>
+                <Link to="/about">
+                    <h5>ABOUT</h5>
+                </Link>
             </div>
         </div>
     )
